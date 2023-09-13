01caae2006eaf6ff5c56220f87ca6a3b62b065cc
k3l5ie <kelsiefung16@gmail.com>
2023-09-13 14:49:24 -0400

    Multiples function

---
+++ src/Multiples.java
@@ -0,0 +1,21 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Multiples {
+
+    public static void main(String[] args) {
+        int multiples = 0;
+
+        for (int i = 0; i < 1000; i++) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 || divisibleBy5) {
+                multiples++;
+            }
+        }
+        System.out.println(multiples);
+    }
+}
